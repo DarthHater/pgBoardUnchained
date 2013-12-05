6523cc1efe73a457b51529910fbb07ba6b89125f
unknown <glenn.mohre@gmail.com>
2013-12-04 21:23:11 -0500

    importuing board_urls, fixed manaage.py

--- pgBoardUnchained/urls.py
+++ pgBoardUnchained/urls.py
@@ -2,22 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from pgBoardUnchained import settings 
+from board import urls as board_urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'vivalavinyl.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^account/login/$', 'board.views.login'),
-    url(r'^account/logout/$', 'board.views.logout'),
-    url(r'^thread/view/(\d+)/$', 'board.views.thread'),
-    url(r'^thread/reply/(?P<pk>\d+)/$', 'board.views.reply', name='reply'),
-    url(r'^thread/new/', 'board.views.post', name='post'),
-    url(r'^thread/post/', 'board.views.new_thread', name='new_thread'),
-    url(r'^thread/view/(\d+)/$', 'board.views.post'),
-    url(r'', 'board.views.list'),
-    url(r'^list/(\d+)/$', 'board.views.list', name='list_threads'),
-
+    url(r'^', include(board_urls))
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
